c928d36bdc21c631c0acc9b9916cb98c6f5fac71
Michael <ppywmjr@gmail.com>
2023-05-19 19:22:58 +0100

    feat: add react testing library and first test

---
+++ __tests__/index.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../app/page";
+import "@testing-library/jest-dom";
+
+describe("Home", () => {
+  it("renders the card headings", () => {
+    render(<Home />);
+
+    const heading = screen.getAllByRole("heading");
+
+    expect(heading[0]).toHaveTextContent("Me and my site ->");
+    expect(heading[1]).toHaveTextContent("The making of ->");
+    expect(heading[2]).toHaveTextContent("Quality topics ->");
+  });
+});
