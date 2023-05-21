376df540fb34d61f77fa67d234a333651a6db0b7
Michael <ppywmjr@gmail.com>
2023-05-21 08:03:07 +0100

    feat update linting

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["next"],
+
+  overrides: [
+    {
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+      extends: ["plugin:testing-library/react"],
+    },
+  ],
+};
