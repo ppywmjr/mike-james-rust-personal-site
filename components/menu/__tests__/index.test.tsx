ec2fb454418dad2b0bebd1ad14f64efbdf397241
Michael <ppywmjr@gmail.com>
2023-05-21 08:04:30 +0100

    feat: add Menu tests and make burger menu a button

--- components/menu/__tests__/index.test.tsx
+++ components/menu/__tests__/index.test.tsx
@@ -1,18 +1,41 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 
 import Menu from "../index";
-import styles from "../../menu-item/MenuItem.module.css";
 
 describe("Home", () => {
-  it("renders the Menu with current URI /me-and-my-site", () => {
+  it("renders the Menu with active URI /me-and-my-site", () => {
     render(<Menu currentUri={"/me-and-my-site"} />);
 
-    const currentMenuItem = screen.getByRole("listitem", {
-      name: /Me and my site/i,
-    });
+    const activeMenuLink = screen.getAllByRole("listitem").find(listitem => listitem.textContent === 'Me and my site');
+    const inactiveMenuLink = screen.getAllByRole("listitem").find(listitem => listitem.textContent === 'Home');
 
-    expect(currentMenuItem).toHaveTextContent("Me and my site");
-    expect(currentMenuItem).toHaveClass(styles.active);
+    expect(activeMenuLink).toHaveClass("active");
+    expect(inactiveMenuLink).not.toHaveClass("active");
+  });
+
+  it("only home link starts not hidden", () => {
+    render(<Menu currentUri={"/me-and-my-site"} />);
+
+    const navBarMenuLink = screen.getAllByRole("listitem").find(listitem => listitem.textContent === 'Home');
+    const subMenuLink = screen.getAllByRole("listitem").find(listitem => listitem.textContent === 'Me and my site');
+
+    expect(navBarMenuLink).not.toHaveClass("hidden");
+    expect(subMenuLink).toHaveClass("hidden");
+  });
+  
+  it("burger menu reveals links", async () => {
+    render(<Menu currentUri={"/me-and-my-site"} />);
+
+    const burger = screen.getByRole("button");
+    const subMenuLink = screen.getAllByRole("listitem").find(listitem => listitem.textContent === 'Me and my site');
+
+    userEvent.click(burger);
+
+    await waitFor(() => {
+      expect(subMenuLink).not.toHaveClass("hidden");
+    })
+
   });
 });
