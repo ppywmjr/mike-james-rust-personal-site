c29b9e66c2668d3c374a55f11d86ac9d15584b08
Michael <ppywmjr@gmail.com>
2023-05-21 08:05:03 +0100

    test add page snapshot tests

--- app/__tests__/page.test.tsx
+++ app/__tests__/page.test.tsx
@@ -1,15 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
 import Home from "../page";
-import "@testing-library/jest-dom";
 
 describe("Home", () => {
-  it("renders the card headings", () => {
-    render(<Home />);
-
-    const heading = screen.getAllByRole("heading");
-
-    expect(heading[0]).toHaveTextContent("Me and my site ->");
-    expect(heading[1]).toHaveTextContent("The making of ->");
-    expect(heading[2]).toHaveTextContent("Quality topics ->");
+  it("renders the page", () => {
+    const tree = renderer.create(<Home />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
