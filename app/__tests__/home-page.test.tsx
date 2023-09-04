578e41d719d49b768668320fb181d58ccf267d5d
Michael <ppywmjr@gmail.com>
2023-09-04 20:36:28 +0100

    test: replace snapshots with react resting library tests (#20)

--- app/__tests__/home-page.test.tsx
+++ app/__tests__/home-page.test.tsx
@@ -1,9 +1,31 @@
-import renderer from "react-test-renderer";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
 import Home from "../page";
 
-describe("Home", () => {
-  it("renders the page", () => {
-    const tree = renderer.create(<Home />).toJSON();
-    expect(tree).toMatchSnapshot();
+describe("Home page", () => {
+  it("renders with h1", () => {
+    render(<Home />);
+
+    const heading = screen.getByRole("heading", { level: 1 });
+
+    expect(heading).toBeVisible();
   });
+  
+  it("renders with img", () => {
+    render(<Home />);
+
+    const image = screen.getByRole("img", { name: /My selfie/ });
+
+    expect(image).toBeVisible();
+  });
+
+  it("renders with link card", () => {
+    render(<Home />);
+
+    const text = screen.getByRole("link", {name: /Me and my site/});
+
+    expect(text).toBeVisible();
+  });  
+
 });
