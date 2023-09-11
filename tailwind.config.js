ac8a5d138f139a6f6b54b6527ca6530d35b226fc
Michael <ppywmjr@gmail.com>
2023-09-11 18:06:27 +0100

    feat: make link text contrast AA accessible (#28)

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
         mjr_very_light_green: "#DDE6DD",
         mjr_dark_blue: "#0D2B57",
         mjr_orange: "#E07A5F",
-        mjr_very_dark_orange: "#B55940",
+        mjr_very_dark_orange: "#b8573d",
         mjr_dark_orange: "#CC6B50",
         mjr_light_orange: "#E1B0A3",
       },
