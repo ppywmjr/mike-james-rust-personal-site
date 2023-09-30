3a53553b44ca1c3c5bbdffffbe17290a7d0aa9e9
Michael <ppywmjr@gmail.com>
2023-09-30 17:04:42 +0100

    more work in progress

--- app/form/SampleForm.tsx
+++ app/form/SampleForm.tsx
@@ -1,31 +1,44 @@
 "use client";
 
 import { useTransition } from "react";
-import { useForm } from "react-hook-form";
-import Form from "@components/form";
-import TextInput from "@components/form/text-input/TextInput";
-import { action, type FormData } from "./action";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+// import Form from "@components/form";
+// import TextInput from "@components/form/text-input/TextInput";
+import { action, FormSchemaType, formSchema } from "./action";
 
 export function SampleForm() {
-  const [isPending, startTransition] = useTransition();
+  // const [isPending, startTransition] = useTransition();
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors },
+  // } = useForm<FormData>();
+
+  // const onSubmit = handleSubmit((data) => {
+  //   startTransition(() => {
+  //     action(data);
+  //   });
+  // });
+
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>();
-
-  const onSubmit = handleSubmit((data) => {
-    startTransition(() => {
-      action(data);
-    });
+    formState: { errors, isSubmitting },
+  } = useForm<FormSchemaType>({
+    resolver: zodResolver(formSchema),
   });
 
+  // const onSubmit: SubmitHandler<FormSchemaType> = (data) => {
+  //   console.log(data);
+  // };
+
   return (
-    <Form action={onSubmit}>
+    <form onSubmit={handleSubmit(action)}>
       {/* <TextInput name="message" label="Message:" />
       <TextInput name="email" label="Email:" isRequired={true} type="email" /> */}
       <label className="block  w-full" htmlFor="email">
-        Name
+        Email
       </label>
       <input
         className="block  w-full border-2 border-pink my-2"
@@ -33,9 +46,11 @@ export function SampleForm() {
         id="email"
         {...register("email", { required: true })}
       />
-      {errors.email && <span>This field is required</span>}
+      {errors.email && (
+        <span className="text-red-800 block mt-2">{errors.email?.message}</span>
+      )}
       <label className="block  w-full" htmlFor="message">
-        Description
+        Message
       </label>
       <input
         className="block w-full border-2 border-pink my-2"
@@ -43,7 +58,18 @@ export function SampleForm() {
         id="message"
         {...register("message", { required: true })}
       />
-      {errors.message && <span>This field is required</span>}
-    </Form>
+      {errors.message && (
+        <span className="text-red-800 block mt-2">
+          {errors.message?.message}
+        </span>
+      )}
+      <button
+        type="submit"
+        disabled={isSubmitting}
+        className="block bg-mjr_orange hover:bg-mjr_very_dark_orange p-2 my-3 w-40 center"
+      >
+        Submit
+      </button>
+    </form>
   );
 }
--- app/form/action.ts
+++ app/form/action.ts
@@ -1,24 +1,36 @@
-"use server";
+// "use server";
 
 import z from "zod";
-import { revalidatePath } from "next/cache";
+import { SubmitHandler } from "react-hook-form";
+// import { revalidatePath } from "next/cache";
 
-export async function action(formData: FormData) {
-  const contactSchema = z.object({
-    email: z.string().email().nonempty(),
-    message: z.string().min(1).max(200),
-  });
+export const formSchema = z.object({
+  email: z.string().email("must be an email").nonempty("cannot be empty"),
+  message: z.string().min(1, "giz a message").max(200),
+});
 
+export type FormSchemaType = z.infer<typeof formSchema>;
+
+
+  // const onSubmit: SubmitHandler<FormSchemaType> = (data) => {
+  //   console.log(data);
+  // };
+
+
+export const action: SubmitHandler<FormSchemaType> = (data) => {
+  // export async function action(formData: FormData) {
+  console.log("in action");
   try {
-    const parse = contactSchema.parse({
-      email: formData.get("email"),
-      message: formData.get("message"),
+    const parse = formSchema.parse({
+      email: data.email,
+      message: data.message,
     });
     console.log(parse);
   } catch (err) {
+    console.log(err);
     if (err instanceof z.ZodError) {
       console.log(err.issues);
     }
   }
-  revalidatePath("/form");
-}
+  // revalidatePath("/form");
+};
