a867cbc609361d1f6450d10428a8060764bb8fe5
Michael <ppywmjr@gmail.com>
2023-10-07 12:34:22 +0100

    feat: clarify that the contact form does not send a message

--- app/forms/contact/page.tsx
+++ app/forms/contact/page.tsx
@@ -6,7 +6,6 @@ import { ContactForm } from "./ContactForm";
 import formsMetaData from "../formsMetadata";
 import NeighbourNavigation from "@components/article/neighbour-navigation";
 
-
 export const metadata = {
   title: "Contact me",
   description: "A sample form",
@@ -28,16 +27,17 @@ export default function Contact() {
         text={"Submit a form, and nothing will happen!"}
       />
       <Paragraph>
-        This form is simply for submitting some data. It is validated on the
-        front end with react-hook-form and the payload is handled by a Next.js
-        13 server action. The front and back end validations use Zod for the
-        validation rules. The browser logs the data sent in the browser console.
-        The back end returns the parsed data back to the browser, and the
-        broswer also logs that. If you open the browser console (Windows: Ctrl +
-        Shift + J. Mac: Cmd + Opt +J) you can see that nothing is submitted
-        until the form is valid. And you can see the front and back end data.
-        Because they are using the same schema to validate, they should be the
-        same.
+        This form is simply for submitting some data. It doesn&rsquo;t actually
+        send a message, it&rsquo;s a demo of client and server side validation.
+        It is validated on the front end with react-hook-form and the payload is
+        handled by a Next.js 13 server action. The front and back end
+        validations use Zod for the validation rules. The browser logs the data
+        sent in the browser console. The back end returns the parsed data back
+        to the browser, and the broswer also logs that. If you open the browser
+        console (Windows: Ctrl + Shift + J. Mac: Cmd + Opt +J) you can see that
+        nothing is submitted until the form is valid. And you can see the front
+        and back end data. Because they are using the same schema to validate,
+        they should be the same.
       </Paragraph>
       <Paragraph>
         The data returned from the server is displayed on the screen
