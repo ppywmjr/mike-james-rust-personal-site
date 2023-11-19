df8e426da0f69e537ed10c6f46f6ed006204f4f8
Michael <ppywmjr@gmail.com>
2023-11-19 18:34:56 +0000

    bug: fix grid gap on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
         height={214}
         priority
       />
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:pt-16">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-2 sm:pt-16">
         {LINK_CARDS.map((item) => (
           <LinkCard
             key={item.target}
